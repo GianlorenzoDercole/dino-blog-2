bf90a5726a9262b9c822a4d14412b017cdee37fc
GianlorenzoDercole <johndercole01@gmail.com>
2022-10-13 14:03:17 -0700

    added state to post

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import React, {Component} from 'react'
-// import Post from './Post.jsx'
-// class App extends Component {
-//   render(){
-//     const post = {
-//       title: 'dinos awesome',
-//       author: 'ste ste',
-//       body: 'check out property',
-//       comments: ['first', ' gp' , 'h t a']
-//     }
+// DINO
 
-//     return (
-//       <div className="App">
-//         <Post post={post} name='April'/>
-
-//       </div>
-//     );
-//     }
-// }
-
-// export default App;
-
-
-
-import React, { Component} from 'react'
-import Roster from './Roster.jsx'
-
-export default class App extends Component {
-
-  students = [{name:'hi'},{name:'hi'},{name:'hi'}]
-
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react'
+import Post from './Post.jsx'
+class App extends Component {
   render(){
+    const post = {
+      title: 'dinos awesome',
+      author: 'ste ste',
+      body: 'check out property',
+      comments: ['first', ' gp' , 'h t a']
+    }
 
     return (
-      <>
-        <Roster students={this.students}/>
-      </>
-    )
-  }
+      <div className="App">
+        <Post post={post} name='April'/>
 
+      </div>
+    );
+    }
 }
+
+export default App;
+
+
+// STUDENT
+// import React, { Component} from 'react'
+// import Roster from './Roster.jsx'
+
+// export default class App extends Component {
+
+//   students = [{name:'hi'},{name:'hi'},{name:'hi'}]
+
+//   render(){
+
+//     return (
+//       <>
+//         <Roster students={this.students}/>
+//       </>
+//     )
+//   }
+
+// }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,11 +1,42 @@
+//NO STATE
+
+// import React, {Component} from 'react'
+
+// import Comment from './Comment'
+// class Post extends Component {
+//     render() {
+//         return (
+//             <>
+//             <h1>{this.props.post.title}</h1>
+//             {this.props.post.comments.map(item => (
+//                 // <li>{item}</li>
+//                 <Comment message={item} />
+//             ))}
+//             </>
+//         )
+//     }
+// }
+
+// export default Post
+
+
+
+// STATE
+
 import React, {Component} from 'react'
 
 import Comment from './Comment'
 class Post extends Component {
+    state = {
+        title: 'dinos awesome',
+        author: 'ste ste',
+        body: 'check out property',
+        comments: ['first', ' gp' , 'h t a']
+    }
     render() {
         return (
             <>
-            <h1>{this.props.post.title}</h1>
+            <h1>{this.state.title}</h1>
             {this.props.post.comments.map(item => (
                 // <li>{item}</li>
                 <Comment message={item} />
