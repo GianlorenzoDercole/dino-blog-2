d427108f13a868c8b7ac58149f1b4d6db788cbe4
GianlorenzoDercole <johndercole01@gmail.com>
2022-10-14 15:07:53 -0700

    added state to app and button to change text

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import './App.css';
 import React, {Component} from 'react'
 import Post from './Post.jsx'
 class App extends Component {
+  state = {
+    title: 'dinos awesome',
+    author: 'ste ste',
+    body: 'check out property',
+    comments: ['first', ' gp' , 'h t a']
+}
+  // create a function that changes the body of state
+  changeBody = () => {
+    console.log('click')
+    const userInput = prompt('what shouls the current be')
+    this.setState({body: userInput})
+  }
   render(){
     const post = {
       title: 'dinos awesome',
@@ -15,7 +27,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Post post={post} name='April'/>
+        <Post post={post} name='April' changeBody={this.changeBody} appState={this.state}/>
 
       </div>
     );
--- src/Post.jsx
+++ src/Post.jsx
@@ -23,24 +23,51 @@
 
 // STATE
 
+// import React, {Component} from 'react'
+
+// import Comment from './Comment'
+// class Post extends Component {
+//     state = {
+//         title: 'dinos awesome',
+//         author: 'ste ste',
+//         body: 'check out property',
+//         comments: ['first', ' gp' , 'h t a']
+//     }
+//     render() {
+//         return (
+//             <>
+//             <h1>{this.state.title}</h1>
+//             {this.props.post.comments.map(item => (
+//                 // <li>{item}</li>
+//                 <Comment message={item} />
+//             ))}
+//             <button onClick={this.props.changeBody}>click</button>
+//             </>
+//         )
+//     }
+// }
+
+// export default Post
+
+
+
+// State in app
+
 import React, {Component} from 'react'
 
 import Comment from './Comment'
 class Post extends Component {
-    state = {
-        title: 'dinos awesome',
-        author: 'ste ste',
-        body: 'check out property',
-        comments: ['first', ' gp' , 'h t a']
-    }
+
     render() {
         return (
             <>
-            <h1>{this.state.title}</h1>
+            <h1>{this.props.post.title}</h1>
+            <p>{this.props.appState.body}</p>
             {this.props.post.comments.map(item => (
                 // <li>{item}</li>
                 <Comment message={item} />
             ))}
+            <button onClick={this.props.changeBody}>click</button>
             </>
         )
     }
