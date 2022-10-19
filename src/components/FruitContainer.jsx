9f5ef433cee2d200b9bdfb2324219dc97aac9bd0
GianlorenzoDercole <johndercole01@gmail.com>
2022-10-19 14:53:38 -0700

    added filtered list to fruit filter

--- src/components/FruitContainer.jsx
+++ src/components/FruitContainer.jsx
@@ -1,5 +1,72 @@
 
 
+// BASIC
+
+// import React, { Component } from 'react'
+// import List from './List'
+// import Input from './Input'
+
+// const starterFruits = [
+//     'mango',
+//     'apple',
+//     'pineapple',
+//     'strawberry'
+// ]
+
+// export default class FruitContainer extends Component {
+
+
+//     // state will live here
+
+//     state = {
+//         // data to display in the list
+//         fruitsToDisplay: starterFruits,
+//         // data inout to use to filter fruits
+//         filterValue: '',
+//
+//     }
+//     // event handlers will live here that manipulate state
+//     handleFilterChange = e => {
+//         // what the user typed in
+//         const filterValue = e.target.value
+//         // loop over the fruits to display and filter and include them if they match what the user typed in
+//         const filteredFruitList = this.state.fruitsToDisplay.filter(fruit => {
+//             // return boolan if true the fruit will be kept
+//             return fruit.toLowerCase().includes(filterValue.toLowerCase())
+
+//         })
+//         this.setState({
+//             filterValue: filterValue,
+//             fruitsToDisplay: filteredFruitList
+
+//         })
+
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <h1>fruit filter</h1>
+//                 {/* pass event handlers to the input */}
+//                 <Input
+//                     handleFilterChange={this.handleFilterChange}
+//                     value={this.state.filterValue}
+
+//                 />
+
+//                 {/* pass data from state to the list */}
+//                 <List
+//                     fruits={this.state.fruitsToDisplay}
+//                 />
+//             </div>
+//         )
+//     }
+// }
+
+
+
+
+// LISTS
+
 import React, { Component } from 'react'
 import List from './List'
 import Input from './Input'
@@ -17,21 +84,29 @@ export default class FruitContainer extends Component {
     // state will live here
 
     state = {
+        // data to display in the list
         fruitsToDisplay: starterFruits,
-        filterValue: ''
+        // data inout to use to filter fruits
+        filterValue: '',
+        //list of everything that didn't match
+        fruitsFiltered: []
     }
     // event handlers will live here that manipulate state
     handleFilterChange = e => {
         // what the user typed in
         const filterValue = e.target.value
         // loop over the fruits to display and filter and include them if they match what the user typed in
-        const filteredFruitList = this.state.fruitsToDisplay.filter(fruit => {
+        const filteredFruitList = starterFruits.filter(fruit => {
             // return boolan if true the fruit will be kept
             return fruit.toLowerCase().includes(filterValue.toLowerCase())
 
         })
+        const fruitsFiltered = starterFruits.filter(fruit => {
+            return !fruit.toLowerCase().includes(filterValue.toLowerCase())
+        })
         this.setState({
             filterValue: filterValue,
+            fruitsFiltered,
             fruitsToDisplay: filteredFruitList
 
         })
@@ -51,6 +126,7 @@ export default class FruitContainer extends Component {
                 {/* pass data from state to the list */}
                 <List
                     fruits={this.state.fruitsToDisplay}
+                    removedFruits={this.state.fruitsFiltered}
                 />
             </div>
         )
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,3 +1,31 @@
+
+
+// Basic
+
+// import React, {Component} from 'react'
+
+
+// export default class List extends Component {
+//   render() {
+//       const fruitItems = this.props.fruits.map((fruit, i) => {
+//           return <li key={`fruitItems${i}`}>{fruit}</li>
+//       })
+
+//     return (
+//         <div>
+//             <ul>
+//                 {fruitItems}
+//             </ul>
+//         </div>
+
+//     )
+//   }
+// }
+
+
+
+//
+
 import React, {Component} from 'react'
 
 
@@ -6,10 +34,19 @@ export default class List extends Component {
       const fruitItems = this.props.fruits.map((fruit, i) => {
           return <li key={`fruitItems${i}`}>{fruit}</li>
       })
+      const removedFruits = this.props.removedFruits.map((fruit, i) => {
+          return <li key={`fruitItems${i}`}>{fruit}</li>
+      })
     return (
         <div>
+            <h2>filtered fruits</h2>
             <ul>
                 {fruitItems}
+            </ul>
+
+            <h2>removed fruits</h2>
+            <ul>
+                {removedFruits}
             </ul>
         </div>
 
